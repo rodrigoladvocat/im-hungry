585a46df7d374699d1acbb1a0945c7f7df38f364
rodrigoladvocat <rrlc@cin.ufpe.br>
2023-08-02 13:54:13 -0300

    .env + ifood_launch funct.

--- web_launch.py
+++ web_launch.py
@@ -4,30 +4,36 @@ from selenium.webdriver.edge.service import Service as EdgeService
 import time
 from selenium.webdriver.support.wait import WebDriverWait
 from log_gmail import pull_code
+from dotenv import load_dotenv, find_dotenv
+import os
 
-service = EdgeService('msedgedriver.exe')
-driver = webdriver.Edge(service=service)
+def launch_ifood(order) -> None:
+    load_dotenv(find_dotenv())
 
-driver.get('https://ifood.com.br/entrar/email')
+    service = EdgeService('msedgedriver.exe')
+    driver = webdriver.Edge(service=service)
 
-element = driver.find_element(By.CLASS_NAME, 'simple-form-input__field')
-element.send_keys('ladvocatrodrigo@gmail.com')
-element.submit()
+    driver.get('https://ifood.com.br/entrar/email')
 
-time.sleep(8)
+    element = driver.find_element(By.CLASS_NAME, 'simple-form-input__field')
+    element.send_keys(os.getenv('email'))
+    element.submit()
 
-ifood_code = pull_code()
+    time.sleep(8)
 
-for i in range(6):
-    code_div = driver.find_element(By.ID, f'otp-input-{i}')
-    code_div.send_keys(ifood_code[i])
-    code_div.submit()
+    ifood_code = pull_code()
 
-time.sleep(5)
+    for i in range(6):
+        code_div = driver.find_element(By.ID, f'otp-input-{i}')
+        code_div.send_keys(ifood_code[i])
+        code_div.submit()
 
-home_addr = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div/div/div/div[1]/div/div/div[2]/div[2]/div[1]/button[1]')
-home_addr.click()
+    time.sleep(5)
 
-time.sleep(3)
+    home_addr = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div/div/div/div[1]/div/div/div[2]/div[2]/div[1]/button[1]')
+    home_addr.click()
 
-driver.quit()
+    time.sleep(3)
+
+    driver.quit()
+    return
